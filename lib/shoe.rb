1926f98b916ec2ff138e41915f7b8621bd1dd5b5
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-20 15:39:27 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor 
+  attr_reader
+  attr_writer
+  
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+end
